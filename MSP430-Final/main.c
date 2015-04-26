820ace07bdef50e7c61052798a20da5e4505f278
jdh747 <joshhayes747@gmail.com>
2015-04-26 13:12:53 +0800

    Initial Commit

---
+++ MSP430-Final/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  MSP430-Final
+//
+//  Created by Joshua Hayes on 26/04/2015.
+//  Copyright (c) 2015 Joshua Hayes. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
